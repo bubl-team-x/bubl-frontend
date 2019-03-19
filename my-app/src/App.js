293dabae4719aa1c3793cc505f0ebf7a95636b5c
Alexnadria Supriana <alexa9410@gmail.com>
2019-03-18 21:16:03 -0400

    Todo List

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import './App.css';
+// import { Route } from "react-router-dom";
+// import { Home, About, Contact, Navigation } from './NavBar/components';
+
+// // Nav-Bar
+// const App = () => (
+//   <div>
+//     <Navigation />
+//     <Route path={ "/" } exact component={ Home } />
+// 	  <Route path={ "/about" } component={ About } />
+// 	  <Route path={ "/contact" } component={ Contact } />
+//   </div>
+// );
+
+import React from 'react';
 import './App.css';
+import TodoList from "./Todo/TodoList";
+import TodoForm from "./Todo/TodoForm";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+// TO-DO
+const todos = [
+    {
+      task: "Read 5 summer listings",
+      id: 1234,
+      completed: false
+    },
+    {
+      task: "Create club",
+      id: 1233,
+      completed: false
+    },
+    {
+      task: "Go to a party",
+      id: 1222,
+      completed: false
+    },
+    {
+      task: "Join fall sport",
+      id: 1256,
+      completed: false
+    }
+  ];
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        todos,
+        newTodoText: ""
+      };
+    }
+    handleChanges = event => {
+      this.setState({ [event.target.name]: event.target.value });
+    };
+  
+    render() {
+      return (
+        <div>
+          <h2>Lets set GoAls!</h2>
+          <TodoList todos={this.state.todos} />
+          <TodoForm
+            handleChanges={this.handleChanges}
+            newTodoText={this.state.newTodoText}
+          />
+  
+        </div>
+      );
+    }
   }
-}
-
+  
 export default App;
---
+++ my-app/src/Todo/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+	import Todo from "./Todo";
+	
+	function TodoList(props) {
+	  return (
+	    <div>
+	      {props.todos.map(todo => {
+	        return <Todo todo={todo} />;
+	      })}
+	    </div>
+	  );
+	}
+	
+	export default TodoList;
